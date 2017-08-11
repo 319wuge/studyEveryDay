224d6ccb961098874808a3c26079844330dfbf57
tonggen <1799735174@qq.com>
2017-08-11 19:49:39 +0800

    搭建框架

--- src/main/java/com/iw/wuge/agentReport/business/service/proxy/StudySOAProxy.java
+++ src/main/java/com/iw/wuge/agentReport/business/service/proxy/StudySOAProxy.java
@@ -1,11 +1,22 @@
 package com.iw.wuge.agentReport.business.service.proxy;
 
+import com.wuge.study.client.StudySOAClient;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 /**
  * Created by Administrator on 2017/8/11 0011.
  */
 @Component
 public class StudySOAProxy {
+    protected Logger logger = Logger.getLogger(getClass());
 
+    @Autowired
+    private StudySOAClient studySOAClient;
+
+    public void  testSoa() {
+        studySOAClient.testContract();
+    }
 }
---
+++ src/test/java/StudyClientTest.java
@@ -0,0 +1,15 @@
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 2017/8/11 0011.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/spring/spring-context.xml")
+public class StudyClientTest {
+
+    @Autowired
+    private
+}
