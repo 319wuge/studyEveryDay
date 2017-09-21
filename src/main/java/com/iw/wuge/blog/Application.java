0ad513be5992fdf0e85d22c4bef4ec6f51868750
tonggen <1799735174@qq.com>
2017-09-21 10:21:27 +0800

    添加tale框架

--- src/main/java/com/iw/wuge/blog/Application.java
+++ src/main/java/com/iw/wuge/blog/Application.java
@@ -2,14 +2,39 @@ package com.iw.wuge.blog;
 
 
 import com.iw.wuge.blog.init.TaleLoader;
+import com.manyi.iw.uum.soa.model.user.User;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static com.blade.Blade.$;
 
-public class Application {
+public class Application{
 
     public static void main(String[] args) throws Exception {
-        TaleLoader.init();
-        $().start(Application.class);
+
+
     }
 
+}
+
+class Tag {
+    Tag(int test) {
+        System.out.println("sub的值：" + test);
+    }
+}
+
+class Card {
+    Tag t1 = new Tag(1);
+
+    Card() {
+        System.out.println("Card()");
+        new Tag(333);
+    }
+    Tag t2 = new Tag(2);
+    void f() {
+        System.out.println("f()");
+    }
+    Tag t3 = new Tag(3);
 }
