1d42edaa751131d3a78927616001f91aeaae899b
1799735174@qq.com <123456>
2018-02-28 17:11:41 +0800

    Lambda表达式使用

--- src/main/java/com/iw/wuge/studyeveryday/bean/controller/TestLambda.java
+++ src/main/java/com/iw/wuge/studyeveryday/bean/controller/TestLambda.java
@@ -5,7 +5,10 @@ import com.iw.wuge.studyeveryday.bean.Worker;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -24,14 +27,36 @@ public class TestLambda {
 
 //        workers.stream().filter(elem -> Collections.sort(elem))
 //        workers.forEach(System.out :: println);
+
 //        Predicate<Worker> contion1 = n -> n.getName().endsWith("3");
 //        Predicate<Worker> contion2 = e -> e.getAge().equals(24);
 //        workers.stream().filter(contion1.or(contion2)).forEach(e -> System.out.println(e.getName()));
+
 //        workers.stream().map(e -> e.getAge() + 1).forEach(System.out :: println);
-//        workers.stream().map(e -> new Worker(e.getId() + 1, e.getWorkYear(), e.getAge(), e.getName()))
-//                .collect(Collectors.toList()).forEach(System.out :: println);
+
+//        List<Worker> newList = workers.stream().map(e -> new Worker(e.getId() + 1, e.getWorkYear(), e.getAge(), e.getName()))
+//                .collect(Collectors.toList());
+//        newList.forEach(System.out :: println);
+
 //        Integer total = workers.stream().map(e -> e.getAge()).reduce((sum, e) -> sum + e).get();
 //        System.out.println(total);
+
+//        workers.stream().filter(e -> e.getAge().equals(23));
+//        workers.forEach(System.out :: println);
+
+//        String str = workers.stream().map(e -> e.getName().toUpperCase()).collect(Collectors.joining(","));
+//        System.out.println(str);
+
+//        workers.stream().map(Worker :: getAge).distinct().forEach(System.out :: println);
+
+//        IntSummaryStatistics start = workers.stream().mapToInt(Worker :: getAge).summaryStatistics();
+//        System.out.println(start.getAverage());
+//        System.out.println(start.getCount());
+//        System.out.println(start.getMax());
+//        System.out.println(start.getSum());
+        Executors.newSingleThreadExecutor();
+
+
     }
 
 }
